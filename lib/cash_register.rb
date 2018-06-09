87e404d6f3a6c9cdefa57d5db219585fec00a8c1
Leticia Molina <letimolina@gmail.com>
2018-06-09 23:20:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,18 +24,27 @@ class CashRegister
   end
 end
 
-def apply_discount
-  if @discount == 0
-    "There is no discount to apply."
-  else
-    @total -= (@total * (@discount / 100.to_f))
-    "After the discount, the total comes to $#{total.to_i}."
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total -= (@total * (@discount / 100.to_f))
+      "After the discount, the total comes to $#{total.to_i}."
+    end
   end
-end
 
-def items
-  @@items
-end
+  def items
+    @@items
+  end
+
+  def void_last_transaction
+    price, quantity = @@last_item[1], price = @@last_item[2]
+    @total -= (price * quantity)
+    @@last_item.clear
+    @@items.slice!(-1, quantity)
+  end
+# end
+
 
 
 
