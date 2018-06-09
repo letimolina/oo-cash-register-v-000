772fefba89f820d23477516ab10643cc9b6ddeeb
Leticia Molina <letimolina@gmail.com>
2018-06-09 22:57:11 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  @@items = []
+  @@last_item = []
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @@items.clear
+  end
+  
+  def discount
+    @discount
+  end
+  
+  def add_item(name, price, quantity=1)
+    @total += (price * quantity)
+    @@last_item[0], @@last_item[1], @@last_item[2] = name, price, quantity
+    while quantity > 0
+    @@items << name
+    quantity -=1
+  end
+end
+  
+end
